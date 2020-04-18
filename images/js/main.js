41ca141d781a876ac69a777410bb5781f4b310d2
Represent7 <54406244+Represent7@users.noreply.github.com>
2020-04-18 19:40:58 +0300

    Add files via upload

---
+++ images/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+$('.slider').slick({
+   nextArrow: '<button class="slick-arrow slick-next"></button>',
+   prevArrow: '<button class="slick-arrow slick-prev"></button>'
+});
+});
+
+
+{/* <img src="/images/prev.svg" alt="prev arrow"> */}
